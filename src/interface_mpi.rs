4169ebad8f23fe252517587fa53f7e705b3e7add
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-01-12 16:31:19 +1000

    Impl essential functions

--- src/interface_mpi.rs
+++ src/interface_mpi.rs
@@ -1,10 +1,29 @@
 extern "C" {
-    fn mpi_hello_world();
+    fn c_mpi_initialized() -> i32;
+    fn c_mpi_init();
+    fn c_mpi_finalize();
 }
 
-pub fn hello_world() {
+pub fn mpi_init() {
     unsafe {
-        mpi_hello_world();
+        c_mpi_init();
+    }
+}
+
+pub fn mpi_finalize() {
+    unsafe {
+        c_mpi_finalize();
+    }
+}
+
+pub fn mpi_initialized() -> bool {
+    unsafe {
+        let res = c_mpi_initialized();
+        if res == 1 {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
@@ -12,10 +31,12 @@ pub fn hello_world() {
 
 #[cfg(test)]
 mod tests {
-    use super::hello_world;
+    use super::{mpi_finalize, mpi_init, mpi_initialized};
 
     #[test]
-    fn hello_world_works() {
-        hello_world();
+    fn essential_functions_work() {
+        mpi_init();
+        assert!(mpi_initialized());
+        mpi_finalize();
     }
 }
