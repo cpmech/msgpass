ef70eae06e7a04e4ce5d7c09d62b2c1106431c71
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-01-12 19:09:36 +1000

    Rename file

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 /// Defines a type alias for the error type as a static string
 pub type StrError = &'static str;
 
-mod interface_mpi;
-pub use crate::interface_mpi::*;
+mod essential;
+pub use crate::essential::*;
