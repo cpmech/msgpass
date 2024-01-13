e05a10bccf8cb011aff7cbd3f31fac25aa9ca5d3
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-01-13 20:44:47 +1000

    Add test

--- run-single-test.bash
+++ run-single-test.bash
@@ -2,6 +2,14 @@
 
 set -e
 
+if [ "$#" -lt 1 ]; then
+	echo
+	echo "Usage:"
+	echo "        $0 test_filekey"
+	echo
+	exit 1
+fi
+
 NP=4
 if [[ "$CI" == "true" ]]; then
     NP=1
@@ -11,7 +19,7 @@ export CARGO_TARGET_DIR="/tmp/msgpass"
 
 EXAMPLES="/tmp/msgpass/debug/examples"
 
-TEST="test_reduce_sum"
+TEST=$1
 
 rm -rf $EXAMPLES
 cargo build --example $TEST
